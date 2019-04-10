f0d601e2b41d57b12b780afd53418065541fc8e4
Teen Tians Go <ricardomlee@gmail.com>
2019-04-10 16:37:26 +0800

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[38]:
-
-
 def scq(max):
     a,b=0,1
     while b<max:
@@ -15,9 +12,6 @@ for i in scq(1000):
     print(i)
 
 
-# In[42]:
-
-
 g=scq(100)
 print(next(g))
 print(next(g))
@@ -27,19 +21,9 @@ print(next(scq(100)))
 print(next(scq(100)))
 
 
-# In[43]:
-
-
 g1=scq(100)
 print(next(g1))
 g2=scq(100)
 print(next(g2))
 g3=scq(100)
 print(next(g3))
-
-
-# In[ ]:
-
-
-
-
